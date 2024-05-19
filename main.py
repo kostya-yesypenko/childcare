dd2f66a63e73c640d8f9756ce1c3ee2802435d08
Kostia <kostaesipenko120@gmail.com>
2024-05-19 13:13:56 +0300

    -

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, url_for, flash, send_file
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, IntegerField
+from wtforms.validators import DataRequired, NumberRange, Length
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +9,14 @@ import seaborn as sns
 import io
 
 app = Flask(__name__, template_folder='pages')
+app.secret_key = 'your_secret_key'
+
+
+class AddCenterForm(FlaskForm):
+    name = StringField('Назва', validators=[DataRequired(), Length(min=1, max=100)])
+    address = StringField('Адреса', validators=[DataRequired(), Length(min=1, max=200)])
+    monthly_fee = FloatField('Місячна плата', validators=[DataRequired(), NumberRange(min=0)])
+    capacity = IntegerField('Вмістимість', validators=[DataRequired(), NumberRange(min=1)])
 
 
 def create_database():
@@ -37,6 +48,17 @@ def add_center(name, address, monthly_fee, capacity):
     conn.close()
 
 
+def update_center(id, name, address, monthly_fee, capacity):
+    conn = sqlite3.connect('childcare.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''UPDATE childcare_centers SET name = ?, address = ?, monthly_fee = ?, capacity = ?
+                      WHERE id = ?''', (name, address, monthly_fee, capacity, id))
+
+    conn.commit()
+    conn.close()
+
+
 @app.route('/')
 def index():
     conn = sqlite3.connect('childcare.db')
@@ -49,16 +71,32 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_center_route():
-    if request.method == 'POST':
-        name = request.form['name']
-        address = request.form['address']
-        monthly_fee = float(request.form['monthly_fee'])
-        capacity = int(request.form['capacity'])
+    form = AddCenterForm()
+    if form.validate_on_submit():
+        add_center(form.name.data, form.address.data, form.monthly_fee.data, form.capacity.data)
+        flash('Дитячий заклад додано успішно!')
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
 
-        add_center(name, address, monthly_fee, capacity)
-        return redirect('/')
 
-    return render_template('add.html')
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_center_route(id):
+    form = AddCenterForm()
+    if request.method == 'GET':
+        conn = sqlite3.connect('childcare.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM childcare_centers WHERE id = ?', (id,))
+        center = cursor.fetchone()
+        conn.close()
+        form.name.data = center[1]
+        form.address.data = center[2]
+        form.monthly_fee.data = center[3]
+        form.capacity.data = center[4]
+    elif form.validate_on_submit():
+        update_center(id, form.name.data, form.address.data, form.monthly_fee.data, form.capacity.data)
+        flash('Інформацію оновлено успішно!')
+        return redirect(url_for('index'))
+    return render_template('edit.html', form=form, center_id=id)
 
 
 @app.route('/search', methods=['GET', 'POST'])
